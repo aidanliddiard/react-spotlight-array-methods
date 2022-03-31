975cb78390a71dc2cc1137d9134b70b0f1d48a28
aidanliddiard <aidanliddiard@gmail.com>
2022-03-31 12:08:50 -0700

    reduce working

--- reduce.js
+++ reduce.js
@@ -2,9 +2,15 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((acc, brand) => [...acc, ...brand.shoes], []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  const ages = dogs.map((dog) => dog.age);
+  const total = ages.reduce((acc, age) => (acc += age), 0);
+  return total / dogs.length;
+};
